36187f8ee88642f0a28b270df194b1e7a4abe136
islf14 <isaiaslflores@gmail.com>
2019-07-29 20:04:48 -0500

    kjh
    kk

--- resources/views/layouts/gema.blade.php
+++ resources/views/layouts/gema.blade.php
@@ -43,12 +43,13 @@
   <!-- jQuery 3 -->
   {{-- <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script> --}}
   <script src="{{asset('bower_components/jquery/dist/jquery-3.4.1.js')}}"></script>
+
   <!-- Bootstrap 3.3.7 -->
-  {{-- <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script> --}}
-  <script src="{{asset('bower_components/bootstrap-4.3.1/js/bootstrap.js')}}"></script>
+  <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+  {{-- <script src="{{asset('bower_components/bootstrap-4.3.1/js/bootstrap.js')}}"></script> --}}
   @yield('script')
   <!-- SlimScroll -->
-  <script src="{{asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+  {{-- <script src="{{asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script> --}}
   <!-- FastClick -->
   <script src="{{asset('bower_components/fastclick/lib/fastclick.js')}}"></script>
   <!-- AdminLTE App -->
@@ -56,10 +57,10 @@
   <!-- AdminLTE for demo purposes -->
   <script src="{{asset('dist/js/demo.js')}}"></script>
   
-  <script>
+  {{-- <script>
     $(document).ready(function () {
       $('.sidebar-menu').tree()
     })
-  </script>
+  </script> --}}
 </body>
 </html>
--- resources/views/persona/index.blade.php
+++ resources/views/persona/index.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('link')
-  <link rel="stylesheet" href="{{asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/b-1.5.6/sl-1.3.0/datatables.min.css"/>
+  {{-- <link rel="stylesheet" href="{{asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}"> --}}
+  {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> --}}
+  {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/b-1.5.6/sl-1.3.0/datatables.min.css"/> --}}
 @endsection
 
 @section('persona')
@@ -31,11 +31,72 @@
       </ol>
     </section>
 
+    <div class="modal fade" id="modal-default">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Default Modal</h4>
+          </div>
+          <div class="modal-body">
+
+            <div class="row">
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Quick Example</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form">
+                  <div class="box-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputFile">File input</label>
+                      <input type="file" id="exampleInputFile">
+
+                      <p class="help-block">Example block-level help text here.</p>
+                    </div>
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox"> Check me out
+                      </label>
+                    </div>
+                  </div>
+                  <!-- /.box-body -->
+
+                  <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+
     <!-- Main content -->
     <section class="content">
       <h3>plantilla</h3>
       <div>
-        <button type="button" class="btn btn-primary">Nuevo</button>
+        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">Nuevo</button>
+        {{-- <button type="button" class="btn btn-primary">Nuevo</button> --}}
       </div>
       <div class="row">
         <div class="col-xs-12">
@@ -84,11 +145,11 @@
 @section('script')
   <!-- DataTables -->
   <script src="{{asset('js/persona.js')}}"></script>
-  <script src="{{asset('bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-  <script src="{{asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+  {{-- <script src="{{asset('bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script> --}}
+  {{-- <script src="{{asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script> --}}
   {{-- <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/b-1.5.6/sl-1.3.0/datatables.min.js"></script> --}}
   
-  <script>
+  {{-- <script>
   $(function () {
     $('#personas').DataTable()
     $('').DataTable({
@@ -100,6 +161,6 @@
       'autoWidth'   : false
     });
   });
-</script>
+</script> --}}
 
 @endsection
