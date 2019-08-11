a9e790d70aadd70d363305b81d048ce4241d1650
islf14 <isaiaslflores@gmail.com>
2019-08-11 17:06:34 -0500

    jhg
    jhgjh

--- public/js/equipo.js
+++ public/js/equipo.js
@@ -1,58 +1,23 @@
 $(document).ready(function() {
 
 
-    var data = [
-        {
-            "name":       "Tiger Nixon",
-            "position":   "System Architect",
-            "salary":     "$3,120",
-            "start_date": "2011/04/25",
-            "office":     "Edinburgh",
-            "extn":       "5421"
-        },
-        {
-            "name":       "Tiger Nixon",
-            "position":   "System Architect",
-            "salary":     "$3,120",
-            "start_date": "2011/04/25",
-            "office":     "Edinburgh",
-            "extn":       "5421"
-        },
-        {
-            "name":       "Tiger Nixon",
-            "position":   "System Architect",
-            "salary":     "$3,120",
-            "start_date": "2011/04/25",
-            "office":     "Edinburgh",
-            "extn":       "5421"
-        },
-        {
-            "name":       "Tiger Nixon",
-            "position":   "System Architect",
-            "salary":     "$3,120",
-            "start_date": "2011/04/25",
-            "office":     "Edinburgh",
-            "extn":       "5421"
-        },
-        {
-            "name":       "Garrett Winters",
-            "position":   "Director",
-            "salary":     "$5,300",
-            "start_date": "2011/07/25",
-            "office":     "Edinburgh",
-            "extn":       "8422"
-        }
-    ]
-
-    $('#equipos').DataTable( {
-        
-        ajax: 'equipos',
-        columns: [
-            { data: 'codigo_pat' },
-            { data: 'nomTipoE' },
-            { data: 'nomDependencia' },
-            { data: 'nomEstado' },
-            { data: 'nomMarca' }
+    $('#equipos').DataTable({
+        buttons: [
+            'copy', 'excel', 'pdf'
         ]
     } );
 });
+
+// {
+//         buttons: [
+//             'copy', 'excel', 'pdf'
+//         ],
+//         ajax: 'equipos',
+//         columns: [
+//             { data: 'codigo_pat' },
+//             { data: 'nomTipoE' },
+//             { data: 'nomDependencia' },
+//             { data: 'nomEstado' },
+//             { data: 'nomMarca' }
+//         ]
+//     } 
--- routes/web.php
+++ routes/web.php
@@ -36,54 +36,30 @@ Route::middleware(['auth'])->group(function () {
 
 
   //Users
-  Route::get('usuario', 'UserController@index')->name('users.index')
-    ->middleware('permiso:users.index');
+  Route::get('usuario', 'UserController@index')->name('users.index')->middleware('permiso:users.index');
+  Route::get('usuario/{id}', 'UserController@show')->name('users.show')->middleware('permiso:users.show');
+  Route::get('usuario/{id}/edit','UserController@edit')->name('users.edit')->middleware('permiso:users.edit');
+  Route::post('usuario/update/{id}', 'UserController@update')->name('users.update')->middleware('permiso:users.edit');
+  Route::delete('usuario/delete/{id}','UserController@destroy')->name('users.destroy')->middleware('permiso:users.destroy');
 
-  Route::get('usuario/{id}', 'UserController@show')->name('users.show')
-    ->middleware('permiso:users.show');
 
-  Route::get('usuario/{id}/edit','UserController@edit')->name('users.edit')
-    ->middleware('permiso:users.edit');
+  Route::get('usuarios','UserController@listusers')->name('users.list')->middleware('permiso:users.index');
 
-  Route::post('usuario/update/{id}', 'UserController@update')->name('users.update')
-		->middleware('permiso:users.edit');
-
-  Route::delete('usuario/delete/{id}','UserController@destroy')->name('users.destroy')
-    ->middleware('permiso:users.destroy');
-
-  Route::get('usuarios','UserController@listusers')->name('users.list')
-    ->middleware('permiso:users.index');
-
-  Route::get('usuarios/test','UserController@test')->name('users.test')
-    ->middleware('permiso:users.test');
+  Route::get('usuarios/test','UserController@test')->name('users.test')->middleware('permiso:users.test');
 
   //Roles
-  Route::get('rol', 'RoleController@index')->name('roles.index')
-    ->middleware('permiso:roles.index');
-
-  Route::get('rol/create', 'RoleController@create')->name('roles.create')
-    ->middleware('permiso:roles.create');
+  Route::get('rol', 'RoleController@index')->name('roles.index')->middleware('permiso:roles.index');
+  Route::get('rol/create', 'RoleController@create')->name('roles.create')->middleware('permiso:roles.create');    
+  Route::post('rol/store', 'RoleController@store')->name('roles.store')->middleware('permiso:roles.create');    
+	Route::get('rol/{role}', 'RoleController@show')->name('roles.show')->middleware('permiso:roles.show');
+  Route::get('rol/{role}/edit', 'RoleController@edit')->name('roles.edit')->middleware('permiso:roles.edit');
+	Route::post('rol/{role}', 'RoleController@update')->name('roles.update')->middleware('permiso:roles.edit');
+  Route::delete('rol/{role}', 'RoleController@destroy')->name('roles.destroy')->middleware('permiso:roles.destroy');
+  
     
-  Route::post('rol/store', 'RoleController@store')->name('roles.store')
-    ->middleware('permiso:roles.create');
-    
-	Route::get('rol/{role}', 'RoleController@show')->name('roles.show')
-		->middleware('permiso:roles.show');
+	Route::get('roles', 'RoleController@listroles')->name('roles.list')->middleware('permiso:roles.index');
 
-  Route::get('rol/{role}/edit', 'RoleController@edit')->name('roles.edit')
-    ->middleware('permiso:roles.edit');
-
-	Route::post('rol/{role}', 'RoleController@update')->name('roles.update')
-    ->middleware('permiso:roles.edit');
-
-	Route::delete('rol/{role}', 'RoleController@destroy')->name('roles.destroy')
-    ->middleware('permiso:roles.destroy');
-    
-	Route::get('roles', 'RoleController@listroles')->name('roles.list')
-		->middleware('permiso:roles.index');
-
-  Route::get('roles/test','RoleController@test')->name('roles.test')
-    ->middleware('permiso:roles.test');
+  Route::get('roles/test','RoleController@test')->name('roles.test')->middleware('permiso:roles.test');
 
     
 
